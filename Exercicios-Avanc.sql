5248f58db000254557202b29811667c2e71164bc
Michele Lima <106484086+Mplimaa@users.noreply.github.com>
2025-04-11 15:54:11 -0300

    Add files via upload

---
+++ Exercicios-Avanc.sql
@@ -0,0 +1,31 @@
+--Exercícios
+
+Exercício 1 - Liste o título dos livros, os nomes dos autores e a categoria, apenas para os livros que ainda não foram devolvidos.
+
+Exercício 2 - Quantos empréstimos foram feitos por cada funcionário, ordenados do maior para o menor?
+
+Exercício 3 - Liste os usuários que mais emprestaram livros (acima de 5 empréstimos).
+
+Exercício 4 - Mostre os livros que nunca foram emprestados.
+
+Exercício 5 - Liste os 3 livros mais emprestados com a quantidade de empréstimos.
+
+Exercício 6 - Liste os empréstimos que duraram mais de 15 dias.
+
+Exercício 7 - Verifique se o livro com ID 5 está disponível usando a função criada.
+
+Exercício 8 - Use a procedure RegistrarEmprestimo para registrar o empréstimo do livro com ID 3 para o usuário 2, pelo funcionário 1.
+
+Exercício 9 - Liste os nomes das editoras e a quantidade de livros publicados por cada uma.
+
+Exercício 10 - Para cada categoria, mostre o total de empréstimos dos livros daquela categoria.
+
+Exercício 11 - Crie uma procedure chamada BuscarLivrosPorCategoria que receba o nome da categoria e retorne os livros pertencentes a essa categoria.
+
+Exercício 12 - Crie uma procedure chamada DevolverLivro que atualize a data de devolução de um empréstimo com base no ID do empréstimo.
+
+Exercício 13 - Crie uma procedure chamada LivrosPorEditora que liste todos os livros de uma editora cujo nome será passado como parâmetro.
+
+Exercício 14 - Crie uma procedure chamada LivrosAtrasados que liste todos os empréstimos cujo prazo de devolução (15 dias) já passou e que ainda não foram devolvidos.
+
+Exercício 15 - Crie uma procedure chamada ResumoEmprestimosUsuario que receba o ID de um usuário e mostre quantos livros ele já emprestou e quantos ainda estão com ele.
